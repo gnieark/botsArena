998ffc4d39f1988fbe7738e58b53a6ca2f359392
Gnieark <gnieark@free.fr>
2015-11-23 21:54:25 +0100

    .

---
+++ html/index.php
@@ -0,0 +1,62 @@
+root@mail2:~# cat /var/www/mail2.ch-boispetit.fr-https/moteur/inc/prepend.php 
+<?php
+#- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of tinaderp.
+#
+# Copyright (C) Gnieark et contributeurs
+# Licensed under the GPL version 3.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/gpl-3.0-standalone.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+//démmarrer une session php
+@session_start();
+
+require_once("../scr/functions.php");
+
+
+
+$arenas=arenas_get_list();
+
+//form submitting
+if (isset($_POST['xd_check']))
+{
+	//vérifier le numero de formulaire
+	if (($_SESSION['xd_check']!=$_POST['xd_check']) AND ($_POST['xd_check'] !="")){
+		erreur ('Something wrong has appen');
+		die;
+	}
+	//call the good act.php
+	if((isset($arenas['current'])) && (file_exists("src/arenas/act.php"))){
+	  require_once("src/arenas/act.php");
+	}else{
+	  require_once("src/default/act.php");
+	}
+
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="ROBOTS" content="INDEX, FOLLOW" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="author" content="Gnieark" />
+	
+	<title></title>
+</head>
+<body>
+  <header>
+  </header>
+  <section>
+  </section>
+  <footer>
+  </footer>
+</body>
+</html>
