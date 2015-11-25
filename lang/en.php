cadfcf602817edb87cf2d26531a75d014ea0d0a4
Gnieark <gnieark@free.fr>
2015-11-25 22:43:34 +0100

    language files

---
+++ lang/en.php
@@ -0,0 +1,5 @@
+<?php
+$lang=array(
+    'SITE_NAME' => 'Artificials Intelligences\' arena',
+    'SITE_DESCRIPTION'  => 'blah blah blah but english'
+);
---
+++ lang/fr.php
@@ -0,0 +1,5 @@
+<?php
+$lang=array(
+    'SITE_NAME' => 'Arène pour intelligences (ou persque) artificielles',
+    'SITE_DESCRIPTION'  => 'blah blah blah mais en francais'
+);
