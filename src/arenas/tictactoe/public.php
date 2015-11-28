70a670836640c51d411b50003445a587f8de1938
Gnieark <gnieark@free.fr>
2015-11-28 15:12:44 +0100

    fix xd_check

--- src/arenas/tictactoe/public.php
+++ src/arenas/tictactoe/public.php
@@ -40,6 +40,6 @@ if(!$postParams){
 			  ?>
 			</select>
 		</p>
-	<p><input type="button" value="Fight!" onclick="tictactoe(document.getElementById('bot1').value,document.getElementById('bot2').value);"></p>
+	<p><input type="button" value="Fight!" onclick="tictactoe(document.getElementById('bot1').value,document.getElementById('bot2').value,<?php echo xd_check_input(2); ?>);"></p>
     </article>
     <article id="fightResult"></article>
