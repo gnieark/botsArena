0ab271fe17087ff742113814d8a8f2cf8025103e
Gnieark <gnieark@free.fr>
2015-11-28 18:21:32 +0100

    typo

--- lang/fr.php
+++ lang/fr.php
@@ -1,6 +1,9 @@
 <?php
 $lang=array(
     'SITE_NAME' => 'Arène à bots',
-    'SITE_DESCRIPTION'  => 'Bienvenue sur cette arène à bots. Plusieurs jeux sont proposés ici. Vous ne devez pas y jouer, mais dévolopper le "bot" qui jouera en votre nom. Ce site permet de faire s\'affronter les bots des différents développeurs',
+    'SITE_DESCRIPTION'  => 
+      '<p>Bienvenue sur cette arène à bots.<br/>
+      Plusieurs jeux sont proposés ici. Vous ne devez pas y jouer, mais dévolopper le "bot" qui jouera en votre nom. 
+      Ce site permet de faire s\'affronter les bots des différents développeurs</p>',
     'HOME'  => 'accueil'
 );
