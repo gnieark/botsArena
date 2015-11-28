3083c8484c4504015a9521a6127124591cb39f36
Gnieark <gnieark@free.fr>
2015-11-28 18:39:01 +0100

    push empty config.php before setup gitignore it

--- lang/fr.php
+++ lang/fr.php
@@ -4,6 +4,6 @@ $lang=array(
     'SITE_DESCRIPTION'  => 
       '<p>Bienvenue sur cette arène à bots.<br/>
       Plusieurs jeux sont proposés ici. Vous ne devez pas y jouer, mais dévolopper le "bot" qui jouera en votre nom. 
-      Ce site permet de faire s\'affronter les bots des différents développeurs</p>',
+      Ce site permet de faire s\'affronter les bots des différents développeurs.</p>',
     'HOME'  => 'accueil'
 );
