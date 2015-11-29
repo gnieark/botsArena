c3b7c160f0797386fdc3b13a5ceda37c994ab11a
gnieark <gnieark@free.fr>
2015-11-30 00:36:55 +0100

    premier jet formulaire

--- src/home.php
+++ src/home.php
@@ -1,2 +1,15 @@
+<article>
 <?php
-echo $lang['SITE_DESCRIPTION'];
+echo $lang['SITE_DESCRIPTION'];?>
+</article>
+<article id="addBot">
+    <h2>Ajouter votre bot</h2>
+    <form method="POST" action="/">
+        <p><label for="botName">Nom de votre Bot: </label><input id="botName" type="text" name="botName" placeholder="votre pseudo par exemple"/></p>
+        <p><label for="botGame">Jeu du bot: </label><select id="botGame" name="botGame"></select></p>
+        <p><label for="botURL">URL du bot:</label><input type="text" name="botURL" id="botURL" phaceholder="http://"/></p>
+        <p>Description:<textarea></textarea></p>
+        <p><label for="email">Votre e-mail (sera utilisé pour valider l'inscription du bot)</label><input type="text" name="email" id="email"/></p>
+        <p><input type="submit" value="Enregistrer mon bot"/></p>       
+    </form>
+</article>
