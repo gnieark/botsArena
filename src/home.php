d89f9918a0d4628a4e8ceefeca409dcdb6d6e3d9
Gnieark <gnieark@free.fr>
2015-11-30 20:59:15 +0100

    tentative de css4

--- src/home.php
+++ src/home.php
@@ -15,7 +15,7 @@ echo $lang['SITE_DESCRIPTION'];?>
 	  ?>
 	  </select></p>
         <p><label for="botURL">URL du bot:</label><input type="text" name="botURL" id="botURL" phaceholder="http://"/></p>
-        <p>Description:<textarea></textarea></p>
+        <p><label>Description:</label><textarea></textarea></p>
         <p><label for="email">Votre e-mail (sera utilisé pour valider l'inscription du bot)</label><input type="text" name="email" id="email"/></p>
         <p><input type="submit" value="Enregistrer mon bot"/></p>       
     </form>
