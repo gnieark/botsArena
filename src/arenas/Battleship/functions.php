bed6efdf442eb664c1d046fe731f391fb6da4bc5
gnieark <gnieark@free.fr>
2015-12-10 20:53:13 +0100

    battle ship functions

---
+++ src/arenas/Battleship/functions.php
@@ -0,0 +1,17 @@
+<?php
+function get_Post_Params($botsCount){
+      $keysBots=array('bot1','bot2');
+      foreach($keysBots as $botKey){
+	if(!isset($_POST[$botKey])){
+	  return false;
+	}
+	if(!is_numeric(($_POST[$botKey]))){
+
+	}
+	if(($_POST[$botKey] < 0) OR ($_POST[$botKey] > $botsCount)){
+	  error(400,"wrong parameters");
+	  die;
+	}
+      }
+      return array('bot1' => $_POST['bot1'],'bot2' => $_POST['bot2']);
+}
