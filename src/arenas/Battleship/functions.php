ad133c5b296554045dcb5254ce13abce9c45d3ec
gnieark <gnieark@free.fr>
2015-12-10 22:40:08 +0100

    liste des parametres battleship

--- src/arenas/Battleship/functions.php
+++ src/arenas/Battleship/functions.php
@@ -15,3 +15,30 @@ function get_Post_Params($botsCount){
       }
       return array('bot1' => $_POST['bot1'],'bot2' => $_POST['bot2']);
 }
+
+function generate_numeric_select($start,$end,$selected,$name,$id){
+    $out="<select";
+    if($name !== ""){
+        $out.=' name="'.$name'"';
+    }
+    if($id !== ""){
+        $out.=' id="'.$id'"';
+    }
+    $out.=">";
+    
+    if($select == -1){
+        for($i=$start; $i <= $end; $i++ ){
+            $out.='<option value="'.$i.'">'.$i.'</option>';
+        }
+    }else{
+        for($i=$start; $i < $selected; $i++ ){
+            $out.='<option value="'.$i.'">'.$i.'</option>';
+        } 
+        $out.='<option value="'.$selected.'" selected="selected">'.$selected.'</option>';
+        for($i=$selected + 1; $i <= $end; $i++ ){
+            $out.='<option value="'.$i.'">'.$i.'</option>';
+        }
+    }
+    return $out."</select>";
+    
+}
--- src/arenas/Battleship/public.php
+++ src/arenas/Battleship/public.php
@@ -14,32 +14,45 @@ if(!$postParams){
 ?>
     <article>
     <h2><?php echo $lang['MAKE_DUEL'];?></h2>
-		<p>
-			<select name="bot1" id="bot1">
-			  <?php
-			    for($i=0;$i<count($bots);$i++){
-			      if($i==$bot1)
-				$selected='selected="selected"';
-			      else
-				$selected='';
-			      
-			      echo '<option value="'.$i.'" '.$selected.'>'.$bots[$i]['name'].'</option>';
-			    }
-			  ?>
-			</select>
-			&nbsp;VS&nbsp;
-			<select name="bot2" id="bot2">
-			  <?php
-			    for($i=0;$i<count($bots);$i++){
-			      if($i==$bot2)
-				$selected='selected="selected"';
-			      else
-				$selected='';
-			      echo '<option value="'.$i.'" '.$selected.'>'.$bots[$i]['name'].'</option>';
-			    }
-			  ?>
-			</select>
-		</p>
+        <p><label for="widthheight">Taille de la grille</label>
+            <em id="widthheight">largeur:
+                <?php echo generate_numeric_select(1,100,10,'gridWidth',''); ?>
+                &nbsp;Hauteur:
+                <?php echo generate_numeric_select(1,100,10,'gridHeight',''); ?>
+            </em>
+        </p>
+        <p><label for="ship1">Nombre de navires de 1 case:</label><?php echo generate_numeric_select(0,10,0,'ship1','ship1'); ?></p>
+        <p><label for="ship2">Nombre de navires de 2 cases:</label><?php echo generate_numeric_select(0,10,1,'ship2','ship2'); ?></p>
+        <p><label for="ship3">Nombre de navires de 3 cases:</label><?php echo generate_numeric_select(0,10,2,'ship3','ship3'); ?></p>
+        <p><label for="ship4">Nombre de navires de 4 cases:</label><?php echo generate_numeric_select(0,10,1,'ship4','ship4'); ?></p>
+        <p><label for="ship5">Nombre de navires de 5 cases:</label><?php echo generate_numeric_select(0,10,1,'ship5','ship5'); ?></p>
+        <p><label for="ship6">Nombre de navires de 6 cases:</label><?php echo generate_numeric_select(0,10,0,'ship6','ship6'); ?></p>
+        <p>
+                <select name="bot1" id="bot1">
+                    <?php
+                    for($i=0;$i<count($bots);$i++){
+                        if($i==$bot1)
+                        $selected='selected="selected"';
+                        else
+                        $selected='';
+                        
+                        echo '<option value="'.$i.'" '.$selected.'>'.$bots[$i]['name'].'</option>';
+                    }
+                    ?>
+                </select>
+                &nbsp;VS&nbsp;
+                <select name="bot2" id="bot2">
+                    <?php
+                    for($i=0;$i<count($bots);$i++){
+                        if($i==$bot2)
+                        $selected='selected="selected"';
+                        else
+                        $selected='';
+                        echo '<option value="'.$i.'" '.$selected.'>'.$bots[$i]['name'].'</option>';
+                    }
+                    ?>
+                </select>
+        </p>
 	<p><input type="button" value="<?php echo $lang['FIGHT']; ?>" onclick="battleship(document.getElementById('bot1').value,document.getElementById('bot2').value,'<?php echo xd_check_input(2); ?>');"></p>
     <div id="fightResult"></div>
 </article>
