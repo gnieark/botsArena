5666d4d10cff187aba46c4fe768009c3d2c44772
Gnieark <gnieark@free.fr>
2015-12-16 10:42:21 +0100

    specs

--- src/arenas/Battleship/functions.php
+++ src/arenas/Battleship/functions.php
@@ -1,4 +1,9 @@
 <?php
+function is_it_possible_to_place_ships_on_grid($gridWidth,$gridHeight,$nbShipsSize1,$nbShipsSize2,$nbShipsSize3,$nbShipsSize4,$nbShipsSize5,$nbShipsSize6){
+  //return false or true
+
+
+}
 function get_Post_Params($botsCount){
       $keysBots=array('bot1','bot2');
       foreach($keysBots as $botKey){
@@ -53,11 +58,7 @@ function get_IA_Response($iaUrl,$postParams){
     curl_close($ch);   
     return htmlentities($output);
 }
-function is_it_possible_to_place_ships_on_grid($gridWidth,$gridHeight,$nbShipsSize1,$nbShipsSize2,$nbShipsSize3,$nbShipsSize4,$nbShipsSize5,$nbShipsSize6){
-  //return false or true
 
-
-}
 
 function place_ship_on_map($x1,$y1,$x2,$y2,$map){
   if ((($x1 <> $x2) && ($y1 <> $y2))
