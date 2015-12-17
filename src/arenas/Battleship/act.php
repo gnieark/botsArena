5cf7dd261589045cf82e3c8edc2298f14e3ed484
gnieark <gnieark@free.fr>
2015-12-17 22:03:19 +0100

    place ships function

--- src/arenas/Battleship/act.php
+++ src/arenas/Battleship/act.php
@@ -119,9 +119,9 @@ switch ($_POST['act']){
 	      list($xStart,$yStart)=explode(",",$startCoord);
 	      list($xEnd,$yEnd)=explode(",",$endCoord);
 	      if($xStart == $xEnd){
-		$long=abs($yStart - $yEnd +1);		
+		$long=abs($yStart - $yEnd) +1;		
 	      }else{
-		$long=abs($xStart - $xEnd +1);
+		$long=abs($xStart - $xEnd) +1;
 	      }
 	      $nbBoatsIwant[$long]-=1;
 	      $grid[$player]=place_ship_on_map($xStart,$yStart,$xEnd,$yEnd,$grid[$player]);
