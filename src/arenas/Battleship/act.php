5093978301d5d4091d49802c5180fbadfb92ca51
Gnieark <gnieark@free.fr>
2015-12-19 12:54:55 +0100

    fix

--- src/arenas/Battleship/act.php
+++ src/arenas/Battleship/act.php
@@ -282,7 +282,7 @@ switch ($_POST['act']){
 	        $result="hit and sunk";
 	        //remove the ship
 	        unset($_SESSION['ships'][$opponent][$shipIndex]);
-		var_dump($_SESSION['ships'][$opponent]);
+		//var_dump($_SESSION['ships'][$opponent]);
 		//win the game?
 		if(count($_SESSION['ships'][$opponent]) == 0){
 		  $result="hit sunk and win";
