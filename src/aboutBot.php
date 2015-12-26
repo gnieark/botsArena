eb7652416cdf36f7646551486195bae99ae03443
gnieark <gnieark@free.fr>
2015-12-26 22:05:07 +0100

    php fix parse error

--- src/aboutBot.php
+++ src/aboutBot.php
@@ -1,5 +1,5 @@
 <article>
 <?php
-echo '<h2>A props de '.htmlentities($_GET['param']).'</h2><p>Inscrit le '$theBot['date_inscription'].'</p><p>'.$theBot['description'].'</p>';
+echo '<h2>A props de '.htmlentities($_GET['param']).'</h2><p>Inscrit le '.$theBot['date_inscription'].'</p><p>'.$theBot['description'].'</p>';
 ?>
 </article>
