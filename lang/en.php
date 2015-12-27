20d703b2dc17b9987de3141194c27e7ffb7ee544
Jordann <jcollin@rmn.com>
2015-12-27 13:53:12 +0100

    Add translation

--- lang/en.php
+++ lang/en.php
@@ -1,18 +1,50 @@
 <?php
-$lang=array(
-    'lang'	=> 'en',
-    'SITE_NAME' => 'bots\'arena',
-    'SITE_DESCRIPTION'  =>       '<p>Welcome on the bots\'arena.<br/><b>this  website is still in dev. it doesn\'t work yet</b><br/>This vhost is a mirror of dev branch <a href="https://github.com/gnieark/botsArena/tree/dev">this github repo</a></p>
-      <p>Many games will be proposed here. You wont play to, but you will developp the bot who will play for you. 
-      This website will be the arena for bots confrontation.</p>',
-    'HOME'  => 'Home page',
-    'VICTORIES' => 'victories',
-    'DRAW'  => 'drawn match',
-    'MAKE_DUEL' => 'Organise a duel',
-    'FIGHT'     => 'Fight!',
-    'DEV-YOUR-OWN-BOT' => 'Program your own bot, and send it on this arena..',
-    'DOC_SPECS_LINKS'   =>'Read the technicals specifications',
-    'ABOUT'	=> 'About',
-    'LEGALS'	=> 'legal terms',
-    'ADD_YOUR_BOT' => 'Add your bot'
+
+$lang = array(
+    'lang'                          =>  'en',
+
+    'SITE_NAME'                     =>  'Bots\' Arena',
+    'SITE_DESCRIPTION'              =>  '<p>
+                                            Welcome on the Bots\' Arena.<br/>
+                                            <b>This website is still in developpement. It doesn\'t work yet.</b><br/>
+                                            This vhost is a mirror of dev branch <a href="https://github.com/gnieark/botsArena/tree/dev">this github repo</a>
+                                        </p>
+                                        <p>
+                                            Many games will be proposed here. You wont play to, but you will developp the bot who will play for you.<br/>
+                                            This website will be the arena for bots confrontation.
+                                        </p>',
+    'HOME'                          =>  'Home',
+
+    'VICTORIES'                     =>  'victories',
+    'DRAW'                          =>  'drawn match',
+
+    'MAKE_DUEL'                     =>  'Organise a duel',
+    'FIGHT'                         =>  'Fight !',
+
+    'DEV-YOUR-OWN-BOT'              =>  'Program your own bot, and send it on this arena.',
+    'DOC_SPECS_LINKS'               =>  'Read the technicals specifications',
+
+    'E_MAIL_ADD_BOT_INTRO'          =>  'A big thank for your participation. Follow the link below to confirm the addition of your bot',
+    'E_MAIL_ADD_BOT_INTRO_HTML'     =>  '<p>
+                                            A big thank for your participation.
+                                        </p>
+                                        <p>
+                                            Just a click on the following link to confirm the addition of the bot
+                                        </p>',
+    'E_MAIL_ADD_BOT_SIGNATURE'      =>  'Gnieark',
+    'E_MAIL_ADD_BOT_SIGNATURE_HTML' =>  '<p>
+                                            <i>Gnieark.</i>
+                                        </p>',
+
+    'ABOUT'                         =>  'About',
+    'LEGALS'                        =>  'Legal terms',
+
+    'ADD_YOUR_BOT'                  =>  'Add your bot',
+    'BOT_NAME'                      =>  'Bot name',
+    'YOUR_ALIAS_FOR_EXEMPLE'        =>  'Your username eg',
+    'BOT_GAME'                      =>  'The game of your bot',
+    'BOT_URL'                       =>  'The URL of your bot',
+    'BOT_DESCRIPTION'               =>  'Description (html code will not be interpreted, URL will be converted into link):',
+    'YOUR_EMAIL_FOR_BOT_VALIDATION' =>  'Your email address (will serve to validate your bot)',
+    'SAVE_BOT'                      =>  'Save'
 );
