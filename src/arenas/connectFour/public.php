bac2ed4a4547ae04053a4a14e9194f419c9a357c
Gnieark <gnieark@free.fr>
2016-05-09 21:39:53 +0200

    purge file

--- src/arenas/connectFour/public.php
+++ src/arenas/connectFour/public.php
@@ -2,14 +2,7 @@
 require_once(__DIR__."/functions.php");
 
 $bots=get_Bots_Array('connectFour');
-$postParams=get_Post_Params(count($bots));
-if(!$postParams){
-  $bot1="";
-  $bot2="";
-}else{
-  $bot1=$postParams['bot1'];
-  $bot2=$postParams['bot2'];
-}
+
 
 ?>
     <article>
