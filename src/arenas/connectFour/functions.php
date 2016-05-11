66a119689c5d4b2f490e89064d97488883d64730
Gnieark <rpasserieu@ch-boispetit.fr>
2016-05-11 12:21:47 +0200

    query to bot

--- src/arenas/connectFour/functions.php
+++ src/arenas/connectFour/functions.php
@@ -1,5 +1,14 @@
 <?php
-
+function get_IA_Response($iaUrl,$postParams){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $iaUrl);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postParams);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);   
+    return htmlentities($output);
+}
 function get_Post_Params($botsCount){
       $keysBots=array('bot1','bot2');
       foreach($keysBots as $botKey){
