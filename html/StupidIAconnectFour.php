9091a4615148b6b8df326abf66f280e5854169c4
Gnieark <rpasserieu@ch-boispetit.fr>
2016-05-11 14:49:38 +0200

    stupidIa

--- html/StupidIAconnectFour.php
+++ html/StupidIAconnectFour.php
@@ -1,3 +1,18 @@
 <?php
-//debug mode
-print_r($_POST);
+/*
+* stupid IA for battle ship
+* choose by random a free column
+*/
+
+$grid=json_decode($_POST['grid']);
+
+$colAvailable=array();
+
+for($i=0;$i<7;$i++){
+  if($colAvailable[5][$i] == ""){
+    $colAvailable[]=$i;
+  }
+}
+shuffle($colAvailable);
+echo $i;
+die;
