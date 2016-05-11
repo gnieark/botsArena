bc4a44894b3bba11b3afb2838ae96fa9bd819f3b
Gnieark <rpasserieu@ch-boispetit.fr>
2016-05-11 11:16:40 +0200

    del useless code

--- src/arenas/connectFour/public.php
+++ src/arenas/connectFour/public.php
@@ -2,15 +2,6 @@
 require_once(__DIR__."/functions.php");
 
 $bots=get_Bots_Array('connectFou');
-$postParams=get_Post_Params(count($bots));
-
-if(!$postParams){
-  $bot1="";
-  $bot2="";
-}else{
-  $bot1=$postParams['bot1'];
-  $bot2=$postParams['bot2'];
-}
 
 ?>
 <article>
@@ -19,12 +10,7 @@ if(!$postParams){
 			<select name="bot1" id="bot1">
 			  <?php
 			    for($i=0;$i<count($bots);$i++){
-			      if($i==$bot1)
-				$selected='selected="selected"';
-			      else
-				$selected='';
-			      
-			      echo '<option value="'.$i.'" '.$selected.'>'.$bots[$i]['name'].'</option>';
+			      echo '<option value="'.$i.'">'.$bots[$i]['name'].'</option>';
 			    }
 			  ?>
 			</select>
@@ -32,11 +18,7 @@ if(!$postParams){
 			<select name="bot2" id="bot2">
 			  <?php
 			    for($i=0;$i<count($bots);$i++){
-			      if($i==$bot2)
-				$selected='selected="selected"';
-			      else
-				$selected='';
-			      echo '<option value="'.$i.'" '.$selected.'>'.$bots[$i]['name'].'</option>';
+			      echo '<option value="'.$i.'">'.$bots[$i]['name'].'</option>';
 			    }
 			  ?>
 			</select>
