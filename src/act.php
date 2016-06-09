b3a28ba68556423ba6b06fd24b9597d4fc93cac2
Gnieark <rpasserieu@ch-boispetit.fr>
2016-06-09 13:54:20 +0200

    debog sql

--- src/act.php
+++ src/act.php
@@ -140,7 +140,18 @@ switch($_POST['act']){
             
             $botUrl = $_POST['botURL'];
         }
-            
+        
+        echo " INSERT INTO bots_modifs( name, game, url, description, date_modification, validate_secret, author_email) VALUES (
+            '".mysqli_real_escape_string($lnMysql,htmlentities($_POST['botName']))."',
+            '".mysqli_real_escape_string($lnMysql,$_POST['botGame'])."',
+            '".mysqli_real_escape_string($lnMysql,$botUrl)."',
+            '".mysqli_real_escape_string($lnMysql,
+                preg_replace('#^(http|https|mailto|ftp)://(([a-z0-9\/\.\?-_=\#@:~])*)#i','<a href="$1://$2">$1://$2</a>'
+                ,nl2br(htmlentities($_POST['botDescription'])))
+            )."',
+            NOW(),
+            '".$secret."',
+            '".mysqli_real_escape_string($lnMysql,$_POST['email'])."'";
             
         mysqli_query($lnMysql,
         " INSERT INTO bots_modifs( name, game, url, description, date_modification, validate_secret, author_email) VALUES (
