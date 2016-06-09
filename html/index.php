0ac12efccc295d4a9ba5160cf9247f6a274a446e
gnieark <gnieark@free.fr>
2016-06-10 00:36:12 +0200

    css

--- html/index.php
+++ html/index.php
@@ -72,7 +72,7 @@ if(isset($_GET['arena'])){
         
         }
         
-        $asideSectionContent.='<li>'.$img.'<a href="/p/aboutBot/'.$sc['id'].'">'.htmlentities($sc['name']).'</a> ELO rank: '.$sc['ELO'].'</li>'; 
+        $asideSectionContent.='<li>'.$img.'&nbsp;<a href="/p/aboutBot/'.$sc['id'].'">'.htmlentities($sc['name']).'</a> ELO rank: '.$sc['ELO'].'</li>'; 
     }
     $asideSectionContent.='</ul><h2>Détail des matchs</h2>';
     
