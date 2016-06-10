a2f20c383b8f34e9797e9c13ab98a73254fe6dbd
Gnieark <rpasserieu@ch-boispetit.fr>
2016-06-10 10:06:46 +0200

    ELO

--- src/functions.php
+++ src/functions.php
@@ -180,7 +180,7 @@ function get_battles_history($game){
 function ELO_get_podium($arena){
     global $lnMysql;
     $podium=array();
-    $rs=mysqli_query($lnMysql,"SELECT id,name,description,ELO FROM bots WHERE game='".$arena."' AND active='1' ORDER BY ELO DESC, name");
+    $rs=mysqli_query($lnMysql,"SELECT id,name,description,ELO FROM bots WHERE game='".substr($arena,0,8)."' AND active='1' ORDER BY ELO DESC, name");
     while($r = mysqli_fetch_row($rs)){
         $podium[]=array(
             'id'            => $r[0],
