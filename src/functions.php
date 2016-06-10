95149f3750b4a7435332048402d299110009b1ef
Gnieark <rpasserieu@ch-boispetit.fr>
2016-06-10 11:10:26 +0200

    debug

--- src/functions.php
+++ src/functions.php
@@ -180,7 +180,6 @@ function get_battles_history($game){
 function ELO_get_podium($arena){
     global $lnMysql;
     $podium=array();
-    echo "SELECT id,name,description,ELO FROM bots WHERE game='".substr($arena,0,10)."' AND active='1' ORDER BY ELO DESC, name";
     $rs=mysqli_query($lnMysql,"SELECT id,name,description,ELO FROM bots WHERE game='".substr($arena,0,10)."' AND active='1' ORDER BY ELO DESC, name");
     while($r = mysqli_fetch_row($rs)){
         $podium[]=array(
