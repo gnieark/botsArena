80c50a58f14ae2e307b8c3e83737cef122c098a4
Gnieark <gnieark@free.fr>
2016-06-10 18:36:47 +0200

    test pagination

--- src/scores.php
+++ src/scores.php
@@ -3,10 +3,10 @@ $hist=get_battles_history($currentArena);
 
 
 foreach($hist as $sc){
-    echo '<h3><a href="/p/aboutBot/'.urlencode(htmlentities($sc['bot1'])).'">'.$sc['bot1'].'</a> VS <a href="/p/aboutBot/'.urlencode(htmlentities($sc['bot2'])).'">'.$sc['bot2'].'</a></h3>
+    echo '<article><h3><a href="/p/aboutBot/'.urlencode(htmlentities($sc['bot1'])).'">'.$sc['bot1'].'</a> VS <a href="/p/aboutBot/'.urlencode(htmlentities($sc['bot2'])).'">'.$sc['bot2'].'</a></h3>
 	<ul>
 	<li>'.$sc['bot1']." ".$lang['VICTORIES'].":".$sc['player1Wins'].'</li>
 	<li>'.$sc['bot2']." ".$lang['VICTORIES'].":".$sc['player2Wins'].'</li>
 	<li>'.$lang['DRAW'].":".$sc['draws'].'</li>
-	</ul>';
+	</ul></article>';
 }
