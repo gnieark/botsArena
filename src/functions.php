0f907106335dcf1144660d7873bc18812ab4d561
Gnieark <rpasserieu@ch-boispetit.fr>
2016-06-28 14:22:18 +0200

    links to ludus

--- src/functions.php
+++ src/functions.php
@@ -304,14 +304,14 @@ function get_unique_id(){
   return $count;
 }
 function get_default_aside_content($currentArena){
-  global $lang;
+  global $lang, $currentArenaArr;
     
     //bla bla
     $asideSectionContent = '<h2>infos:</h2><p>'.$lang['DEV-YOUR-OWN-BOT'].'<br/> <a href="/'.$currentArena.'/doc">'.$lang['DOC_SPECS_LINKS'].'</a></p>';
     
     //lien pour le ludus
-    if(isset($currentArena['ludusUrl'])){
-      echo '<p><a href="'.$currentArena['ludusUrl'].'">'.$lang['GO-TO-LUDUS'].'</a>&nbsp;'.$lang['LUDUS-DETAIL'].'</p>';
+    if(isset($currentArenaArr['ludusUrl'])){
+      echo '<p><a href="'.$currentArenaArr['ludusUrl'].'">'.$lang['GO-TO-LUDUS'].'</a>&nbsp;'.$lang['LUDUS-DETAIL'].'</p>';
     }
     
     //scores
