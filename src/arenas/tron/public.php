a532b3468f37207c03b7b8e050c26a20bc01ac0a
Gnieark <rpasserieu@ch-boispetit.fr>
2016-06-29 13:49:38 +0200

    try to draw 1000*1000 svg grig

--- src/arenas/tron/public.php
+++ src/arenas/tron/public.php
@@ -33,7 +33,7 @@ $bots=get_Bots_Array('tron');
 			</select>
 		</p>
 		<p><input type="checkbox" id="fullLogs"/><label for="fullLogs">view the full logs</label></p>
-	<p><input id="fightButton" type="button" value="<?php echo $lang['FIGHT']; ?>" onclick="tron(document.getElementById('bot1').value,document.getElementById('bot2').value,'<?php echo xd_check_input(2); ?>',0);"></p>
+	<p><input id="fightButton" type="button" value="<?php echo $lang['FIGHT']; ?>" onclick="tron(document.getElementById('bot1').value,document.getElementById('bot2').value,'<?php echo xd_check_input(2); ?>');"></p>
 	 <div id="fightResult"></div>
 
 </article>
